cc15e709d3e5e80e15f2ae17f420bf12b16d88e3
GaracLui <158767416+GaracLui@users.noreply.github.com>
2026-02-19 14:00:04 -0300

    refactor: update admin.py

--- geek_commerce/shop/admin.py
+++ geek_commerce/shop/admin.py
@@ -6,7 +6,7 @@ from .models import Category, Brand, Product, ProductVariant, ProductImage
 
 # Register your models here.
 # --- CONFIGURACIÓN DE INLINES (Tablas dentro de otras tablas) ---
-class ProductImageInline(admin.TabularInline):
+class ProductImageInline(admin.StackedInline):
     model = ProductImage
     extra = 1 # Cuántos espacios vacíos mostrar por defecto
     readonly_fields = ['image_preview'] # Opcional: para ver la foto cargada
@@ -17,10 +17,13 @@ class ProductImageInline(admin.TabularInline):
         return "No image"
     image_preview.short_description = "Vista Previa"
 
-class ProductVariantInline(admin.TabularInline):
+class ProductVariantInline(admin.StackedInline):
     model = ProductVariant
     extra = 1  # Muestra 1 fila vacía para agregar variantes rápido
     show_change_link = True # Permite ir a la edición completa de la variante
+    formfield_overrides = {
+        JSONField: {'widget': JSONEditorWidget},
+    }
 
 # --- CONFIGURACIÓN DE LOS PANELES PRINCIPALES ---
 
@@ -29,32 +32,30 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent', 'slug')
     list_filter = ('parent',)
     search_fields = ('name',)
-    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     search_fields = ('name',)
-    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'is_active', 'created_at')
+    list_display = ('name', 'category', 'slug', 'is_active', 'created_at')
     list_filter = ('category', 'is_active', 'created_at')
     search_fields = ('name', 'description')
-    prepopulated_fields = {'slug': ('name',)}
 
     formfield_overrides = {
         JSONField: {'widget': JSONEditorWidget},
     }
-    inlines = [ProductVariantInline] # Aquí conectamos las variantes al producto
+    inlines = [
+        ProductVariantInline,
+    ]
 
 @admin.register(ProductVariant)
 class ProductVariantAdmin(admin.ModelAdmin):
-    list_display = ('name', 'product', 'sku', 'brand','price', 'is_master')
+    list_display = ('name', 'product', 'slug', 'sku', 'brand','price', 'is_master')
     list_filter = ('product', 'is_master', 'brand', 'created_at')
-    search_fields = ('name', 'description')
-    prepopulated_fields = {'slug': ('product__slug', 'name')}
+    search_fields = ('name', 'description', 'product__name', 'brand__name')
 
     formfield_overrides = {
         JSONField: {'widget': JSONEditorWidget},
@@ -65,7 +66,7 @@ class ProductVariantAdmin(admin.ModelAdmin):
 class ProductImageAdmin(admin.ModelAdmin):
     list_display = ('variant', 'image_preview', 'alt_text', 'is_main')
     list_filter = ('variant', 'is_main')
-    search_fields = ('variant__name', 'is_main')
+    search_fields = ('variant__name', 'is_main', 'alt_text')
     readonly_fields = ['image_preview']
 
     def image_preview(self, obj):
@@ -73,3 +74,9 @@ class ProductImageAdmin(admin.ModelAdmin):
             return mark_safe(f'<img src="{obj.image.url}" style="height: 50px; border-radius: 5px;" />')
         return "No image"
     image_preview.short_description = "Vista Previa"
+
+
+admin.site.site_header = "Geek Commerce Admin"
+admin.site.site_title = "Geek Commerce Admin Portal"
+admin.site.index_title = "Bienvenido al Panel de Administración de Geek Commerce"
+
